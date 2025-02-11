cc44869a478a655643c15120a9ffbb8580656f25
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-02-11 13:12:13 +0100

    akkjf

--- coga_linux/transform.sh
+++ coga_linux/transform.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 # Autor: Chatgpt
+#  UNTESTED!!!
 
-# Directorio base (por defecto el actual)
-DIRECTORIO="${1:-.}"
-
-# Buscar y reemplazar en todos los archivos .c y .h
-find "$DIRECTORIO" -type f \( -name "*.c" -o -name "*.h" \) -exec  \
+find . -type f \( -name "*.c" -o -name "*.h" \) -exec  \
 sed -i \
 -e 's|#include <glad.h>|#include <glad/glad.h>|g' \
 -e 's|#include <glfw3.h>|#include <GLFW/glfw3.h>|g' \
